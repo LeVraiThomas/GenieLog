f25acf7183f18794157e032219af6395fdf55520
Caro-Chaib <caroline.chaibainou@alumni.univ-avignon.fr>
2019-12-02 15:38:17 +0100

    Function returning titles implemented

--- parser.py
+++ parser.py
@@ -1,1 +1,34 @@
+import os
 
+def getTitle(file_to_parse) :
+	title = ""
+	
+	#Creating directory to put .txt files
+	os.system('rm -r ParsedPapers')
+	os.system('mkdir ParsedPapers')
+	
+	#Calculate the .pdf file to convert
+	file_to_open = 'Papers/' + file_to_parse + '.pdf'
+	file_to_open = file_to_open.replace(' ', '\ ')
+	
+	#Calculate the .txt file to read
+	file_to_read = 'ParsedPapers/' + file_to_parse + '.txt'
+	temp = file_to_read
+	file_to_read = file_to_read.replace(' ', '\ ')
+	
+	#Converting file
+	command = 'pdf2txt ' + file_to_open + ' > ' + file_to_read
+	os.system(command)
+	
+	#Reading .txt file
+	f = open(temp, 'r')
+	first_lines = f.readline()
+	
+	return(first_lines)
+
+
+def main() :
+	print(getTitle('Alexandrov_2015_A Modified Tripartite Model for Document Representation in Internet Sociology'))
+
+
+main()
