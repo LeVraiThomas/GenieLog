776850646a2477f5d75546cfc17e0035f31e12eb
Zkr0 <makrini.zak@gmail.com>
2019-12-02 15:44:00 +0100

    retrieving pdf name successfully

--- parser.py
+++ parser.py
@@ -1,1 +1,17 @@
+import glob
+import os.path
+
+from os.path import basename, splitext
+
+
+
+ListeDeFichierPdf=[] 
+l = glob.glob('Papers/*.pdf')
+for i in l: 
+	a=i.strip("Papers/")
+	ListeDeFichierPdf.append(a)
+
+fichier = open("fichier1.txt", "w")
+fichier.write(ListeDeFichierPdf[0])
+fichier.close()
 
