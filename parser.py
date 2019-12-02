5aa4d6196b5745286db674e0a3f4e9c41403422d
Valentin PARRILLA <valentin.parrilla1@gmail.com>
2019-12-02 14:35:47 +0100

    parser master

--- parser.py
+++ parser.py
@@ -1,1 +1,25 @@
+import os
+
+def parser(src, dst):
+	for line in src: 
+		print(line)
+
+def main():
+	src = "Alexandrov"
+	fichier = "Papers/" + src + ".pdf"
+	cmd = 'pdf2txt ' + fichier + '>' + "Papers/" + src + ".txt"
+	os.system(cmd)
+	txt = "Papers/" + src + ".txt"
+	source = open(txt, "r")
+	destination = open("out.txt", "w")
+	parser(source, destination)
+	try:
+		parser(source, destination)
+    
+	finally:
+		destination.close()
+		source.close()
+
+
+main()
 
