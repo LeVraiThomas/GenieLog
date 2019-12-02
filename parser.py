eaf05689038e364f32441e2c6dd13ef7a1047cc6
AntoineOlivero <uapv1800818@ecv-2w21a-ceri.univ-avignon.fr>
2019-12-02 15:27:07 +0100

    Affichage de l'abstact

--- parser.py
+++ parser.py
@@ -1,17 +1,16 @@
 
 import re
 f = open('Papers/Papers/Alexandrov.txt', "r+")
-content = f.readl()
+content = f.read()
+
 
 debutAbstract = (content.find("Abstract"))
 print(debutAbstract)
 
 finAbstract = (content.find("\n\n", debutAbstract))
 print(finAbstract)
-i = 1
 
-for x in range(debutAbstract,finAbstract):
-    print(debutAbstract)
-    debutAbstract=debutAbstract+1
+
+substring = content[debutAbstract:finAbstract]
+print(substring)
     
-
