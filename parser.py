5d51ff2db4b9e3e986e16e4898461ceba28fdd1b
AntoineOlivero <uapv1800818@ecv-2w21a-ceri.univ-avignon.fr>
2019-12-02 15:09:27 +0100

    Donne le numero de caractere ou débute le mot abstract au numero de caratere de la fin du paragraphe

--- parser.py
+++ parser.py
@@ -1,1 +1,17 @@
 
+import re
+f = open('Papers/Papers/Alexandrov.txt', "r+")
+content = f.readl()
+
+debutAbstract = (content.find("Abstract"))
+print(debutAbstract)
+
+finAbstract = (content.find("\n\n", debutAbstract))
+print(finAbstract)
+i = 1
+
+for x in range(debutAbstract,finAbstract):
+    print(debutAbstract)
+    debutAbstract=debutAbstract+1
+    
+
