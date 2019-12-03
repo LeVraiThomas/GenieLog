5490538b39b17366d04414bb168b1e6403105ebd
Caroline CHAIBAINOU <caroline.chaibainou@alumni.univ-avignon.fr>
2019-12-03 19:19:51 +0100

    All functions implemented and writing into ParsedPapers/ directory

--- parser.py
+++ parser.py
@@ -6,70 +6,46 @@ from os.path import basename, splitext
 	
 def parser():
 	#Creating directory to put .txt files
+	os.system('rm -r ConvertedPapers')
+	os.system('mkdir ConvertedPapers')
 	os.system('rm -r ParsedPapers')
 	os.system('mkdir ParsedPapers')
 	ListeDeFichierPdf=getName()
 	for x in ListeDeFichierPdf:
-		file_to_open = x
-		file_to_open = file_to_open.replace(' ', '\ ')
-		file_to_read = 'ParsedPapers/' + file_to_open.strip('.pdf') + '.txt'
-		temp = file_to_read
-		command = 'pdf2txt ' + file_to_open + ' > ' + file_to_read
-		os.system(command)
-		f = open(temp, 'r+')
-		#Name
-		parsed_file = "Parsed" + i + ".txt"
-		fichier = open(parsed_file, 'w+')
-		fichier.write(ListeDeFichierPdf[i])
-		#Title
-		first_lines = f.readline()
-		
-		fichier.write(first_lines) 
-		#Abstract
-		content = f.read()
-		debutAbstract = (content.find("Abstract"))
-		finAbstract = (content.find("\n\n", debutAbstract))
-		substring = content[debutAbstract:finAbstract]
-		fichier.write(substring) 
-		fichier.close()
+	    x = x.strip('.pdf')
+	    file_to_open = 'Papers/' + x + '.pdf'
+	    file_to_open = file_to_open.replace(' ', '\ ')
+	    file_to_read = 'ConvertedPapers/' + x + '.txt'
+	    temp = file_to_read
+	    file_to_read = file_to_read.replace(' ', '\ ')
+	    command = 'pdf2txt ' + file_to_open + ' > ' + file_to_read
+	    os.system(command)
+	    open_read = open(temp, 'r+')
+	    #Name
+	    parsed_file = 'ParsedPapers/' + x + '.txt'
+	    open_write = open(parsed_file, 'w+')
+	    open_write.write("Nom du fichier : "+x+"\n")
+	    #Title
+	    getTitle(open_read, open_write)
+	    #Abstract
+	    getAbstract(open_read, open_write)
+	    #Close
+	    open_read.close()
+	    open_write.close()
 	
 
 	
-def getTitle(file_to_parse) :
-	title = ""
-	
-	#Creating directory to put .txt files
-	os.system('rm -r ParsedPapers')
-	os.system('mkdir ParsedPapers')
-	
-	#Calculate the .pdf file to convert
-	file_to_open = 'Papers/' + file_to_parse + '.pdf'
-	file_to_open = file_to_open.replace(' ', '\ ')
-	
-	#Calculate the .txt file to read
-	file_to_read = 'ParsedPapers/' + file_to_parse + '.txt'
-	temp = file_to_read
-	file_to_read = file_to_read.replace(' ', '\ ')
-	
-	#Converting file
-	command = 'pdf2txt ' + file_to_open + ' > ' + file_to_read
-	os.system(command)
-	
-	#Reading .txt file
-	f = open(temp, 'r')
-	first_lines = f.readline()
-	
-	return(first_lines)
+def getTitle(f1, f2) :
+    first_lines = f1.readline()
+    f2.write("Titre de l'article : "+first_lines)
+    
 
-def getAbstract():
-	f = open('Papers/Papers/Alexandrov.txt', "r+")
-	content = f.read()
+def getAbstract(f1, f2):
+	content = f1.read()
 	debutAbstract = (content.find("Abstract"))
-	print(debutAbstract)
 	finAbstract = (content.find("\n\n", debutAbstract))
-	print(finAbstract)
 	substring = content[debutAbstract:finAbstract]
-	print(substring)
+	f2.write("Abstract/Resume de l'article : "+substring+"\n") 
 	
 def getName():
 	ListeDeFichierPdf=[] 
