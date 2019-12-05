98cb8601bdd229351a93c1de60c5e256b416919c
Caro-Chaib <caroline.chaibainou@alumni.univ-avignon.fr>
2019-12-05 09:22:36 +0100

    Choice between .txt and .xml implemented

--- parser.py
+++ parser.py
@@ -3,13 +3,18 @@ import glob
 import os.path
 import re
 from os.path import basename, splitext
+import sys
 	
-def parser():
+def init():
 	#Creating directory to put .txt files
 	os.system('rm -r ConvertedPapers')
 	os.system('mkdir ConvertedPapers')
 	os.system('rm -r ParsedPapers')
 	os.system('mkdir ParsedPapers')
+	
+	
+def parserTXT():
+	init()
 	ListeDeFichierPdf=getName()
 	for x in ListeDeFichierPdf:
 	    x = x.strip('.pdf')
@@ -33,8 +38,31 @@ def parser():
 	    open_read.close()
 	    open_write.close()
 	
-
-	
+def parserXML():
+	init()
+	ListeDeFichierPdf=getName()
+	for x in ListeDeFichierPdf:
+	    x = x.strip('.pdf')
+	    file_to_open = 'Papers/' + x + '.pdf'
+	    file_to_open = file_to_open.replace(' ', '\ ')
+	    file_to_read = 'ConvertedPapers/' + x + '.txt'
+	    temp = file_to_read
+	    file_to_read = file_to_read.replace(' ', '\ ')
+	    command = 'pdf2txt ' + file_to_open + ' > ' + file_to_read
+	    os.system(command)
+	    open_read = open(temp, 'r+')
+	    #Name
+	    parsed_file = 'ParsedPapers/' + x + '.xml'
+	    open_write = open(parsed_file, 'w+')
+	    open_write.write("Nom du fichier : "+x+"\n")
+	    #Title
+	    getTitle(open_read, open_write)
+	    #Abstract
+	    getAbstract(open_read, open_write)
+	    #Close
+	    open_read.close()
+	    open_write.close()
+	    
 def getTitle(f1, f2) :
     first_lines = f1.readline()
     f2.write("Titre de l'article : "+first_lines)
@@ -56,7 +84,10 @@ def getName():
 	return(ListeDeFichierPdf)
 
 def main():
-	parser()
+	if len(sys.argv) < 2 : print("Veuillez signifier une option pour convertir correctement votre fichier (-t pour .txt et -x pour .xml)")
+	elif sys.argv[1]=="-t" : parserTXT()
+	elif sys.argv[1]=="-x" : parserXML()
+	else : print("Veuillez signifier une option pour convertir correctement votre fichier (-t pour .txt et -x pour .xml)")
 	
 main()
 
