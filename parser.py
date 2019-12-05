8ba3aece39ba879bffdcb054d5fa87cd854202e1
Valentin PARRILLA <valentin.parrilla1@gmail.com>
2019-12-05 10:09:53 +0100

    Creating all xml tag

--- parser.py
+++ parser.py
@@ -4,6 +4,7 @@ import os.path
 import re
 from os.path import basename, splitext
 import sys
+from lxml import etree
 	
 def init():
 	#Creating directory to put .txt files
@@ -31,9 +32,11 @@ def parserTXT():
 	    open_write = open(parsed_file, 'w+')
 	    open_write.write("Nom du fichier : "+x+"\n")
 	    #Title
-	    getTitle(open_read, open_write)
+	    titre = getTitle(open_read)
+	    open_write.write("Titre de l'article : "+titre)
 	    #Abstract
-	    getAbstract(open_read, open_write)
+	    abstract = getAbstract(open_read)
+	    open_write.write("Abstract/Resume de l'article : "+abstract+"\n") 
 	    #Close
 	    open_read.close()
 	    open_write.close()
@@ -41,39 +44,52 @@ def parserTXT():
 def parserXML():
 	init()
 	ListeDeFichierPdf=getName()
+	
 	for x in ListeDeFichierPdf:
-	    x = x.strip('.pdf')
-	    file_to_open = 'Papers/' + x + '.pdf'
-	    file_to_open = file_to_open.replace(' ', '\ ')
-	    file_to_read = 'ConvertedPapers/' + x + '.txt'
-	    temp = file_to_read
-	    file_to_read = file_to_read.replace(' ', '\ ')
-	    command = 'pdf2txt ' + file_to_open + ' > ' + file_to_read
-	    os.system(command)
-	    open_read = open(temp, 'r+')
-	    #Name
-	    parsed_file = 'ParsedPapers/' + x + '.xml'
-	    open_write = open(parsed_file, 'w+')
-	    open_write.write("Nom du fichier : "+x+"\n")
-	    #Title
-	    getTitle(open_read, open_write)
-	    #Abstract
-	    getAbstract(open_read, open_write)
-	    #Close
-	    open_read.close()
-	    open_write.close()
+		article = etree.Element("article")
+		preamble = etree.SubElement(article, "preamble")
+		titre = etree.SubElement(article, "titre")
+		auteur = etree.SubElement(article, "auteur")
+		abstract = etree.SubElement(article, "abstract")
+		biblio = etree.SubElement(article, "biblio")
+		x = x.strip('.pdf')
+		file_to_open = 'Papers/' + x + '.pdf'
+		file_to_open = file_to_open.replace(' ', '\ ')
+		file_to_read = 'ConvertedPapers/' + x + '.txt'
+		temp = file_to_read
+		file_to_read = file_to_read.replace(' ', '\ ')
+		command = 'pdf2txt ' + file_to_open + ' > ' + file_to_read
+		os.system(command)
+		open_read = open(temp, 'r+')
+		#Name
+		preamble.text = x
+		parsed_file = 'ParsedPapers/' + x + '.xml'
+		#Title
+		title = getTitle(open_read)
+		titre.text = title
+		#Abstract
+		a = getAbstract(open_read)
+		abstract.text = a
+		tree = etree.ElementTree(article)
+		#Close
+		tree.write(parsed_file)
+		open_read.close()
 	    
-def getTitle(f1, f2) :
+
+	    
+	    
+def getTitle(f1) :
     first_lines = f1.readline()
-    f2.write("Titre de l'article : "+first_lines)
+    return first_lines
     
 
-def getAbstract(f1, f2):
+def getAbstract(f1):
 	content = f1.read()
 	debutAbstract = (content.find("Abstract"))
 	finAbstract = (content.find("\n\n", debutAbstract))
 	substring = content[debutAbstract:finAbstract]
-	f2.write("Abstract/Resume de l'article : "+substring+"\n") 
+	return substring
+	
 	
 def getName():
 	ListeDeFichierPdf=[] 
